5ca3c286f533652f53637929bde684e4e7c89bed
Artur Drzyzga <artek.drzyzga@gmail.com>
2021-10-05 11:30:25 +0200

    create new migration with users and add new usercontorller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :articles
+  #resources :users
   get 'about', to: 'pages#about'
  
   root to:'pages#home'
   resources :articles, only: [:show]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
