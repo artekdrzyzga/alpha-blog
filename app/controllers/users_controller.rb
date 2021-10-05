b47232d8d6128de7693abca4e928e7b5df699ef0
Artur Drzyzga <artek.drzyzga@gmail.com>
2021-10-05 19:06:16 +0200

    add update functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
   
   def  new
     @user = User.new
+  end
+
+  def edit
   end
   
   def create
@@ -15,6 +19,18 @@ class UsersController < ApplicationController
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to articles_path, notice: "User was successfully updated." }
+        #format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        #format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
