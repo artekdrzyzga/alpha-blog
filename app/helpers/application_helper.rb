e1ca3892cecdecd8b082bcbff89e8cba00843845
Artur Drzyzga <artek.drzyzga@gmail.com>
2021-10-06 07:12:00 +0200

    add gravatar pictures for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 80})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravatr_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatr_url, alt: user.username, class: "rounded shadow shadow-warning mx-auto d-block")
+  end
 end
