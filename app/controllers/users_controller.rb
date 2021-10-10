ff4968f4d054321b655003855fa20e1e769d1250
Artur Drzyzga <artek.drzyzga@gmail.com>
2021-10-10 13:22:33 +0200

    add admin field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
-  
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  def index 
+    #@users = User.all
+    @users = User.paginate(page: params[:page], per_page: 3)
+  end 
+
   def  new
     @user = User.new
   end
@@ -16,8 +22,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
         # flash[:notice] = "Welcome to Alpha Blog"
-        format.html {redirect_to articles_path, notice: "User #{@user.username} was successfully created"}
+        format.html {redirect_to @user, notice: "User #{@user.username} was successfully created"}
         #format.html { redirect_to @user, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
@@ -30,13 +37,19 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to articles_path, notice: "User was successfully updated." }
+        format.html { redirect_to @user, notice: "User was successfully updated." }
         #format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit, status: :unprocessable_entity }
         #format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to root_path , notice: "Account and all associated articles successfully deleted"
   end
 
   private
@@ -48,6 +61,12 @@ class UsersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user
+        redirect_to @user, notice: "You can edit only your own account"
+      end
     end
 
 
